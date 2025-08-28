d1061709f433036610556a6c23c3d360634be0cd
Elias, Cesar [CUSCA] <cesar.elias@bancocuscatlan.com>
2025-08-27 22:43:06 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_app_cidr" {
+  description = "CIDR block for application VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_rds_cidr" {
+  description = "CIDR block for RDS VPC"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
